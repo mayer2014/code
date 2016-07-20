69b53f26ff5548060189bde2a3d97154ace3ddff
mayer <841439703@qq.com>
2016-07-20 00:40:51 -0700

    h

--- hash.php
+++ hash.php
@@ -12,6 +12,7 @@ $redis->auth('123456');
 
 
 /*
+ * hSet
  * 将哈希表key中的域field的值设为value。如果key不存在，一个新的哈希表被创建并进行HSET操作。如果域field已经存在于哈希表中，旧值将被覆盖。
  *
  * @param string key 哈希表key
@@ -23,6 +24,7 @@ $redis->hSet('member', 'name', 'nick');
 var_dump($redis->hGet('member', 'name'));
 
 /*
+ * hSetNx
  * 将哈希表key中的域field的值设置为value，当且仅当域field不存在。若域field已经存在，该操作无效。如果key不存在，将创建一张新的哈希表。
  *
  * @param string key 哈希表key
@@ -34,6 +36,7 @@ $redis->hSetNx('member', 'name', 'tony');
 var_dump($redis->hGet('member', 'name'));
 
 /*
+ * hLen
  * 返回哈希表key中域的数量，如果key不存在则返回0。 
  *
  * @param string key 哈希表key
@@ -42,6 +45,7 @@ var_dump($redis->hGet('member', 'name'));
 var_dump($redis->hLen('member'));
 
 /*
+ * hDel
  * 删除哈希表key中的一个或多个指定域，不存在的域将被忽略。
  *
  * @param string key 哈希表key
@@ -56,6 +60,7 @@ $redis->hSet('member', 'sex', 'man');
 $redis->hSet('member', 'mobile', '15920370150');
 
 /*
+ * hKeys
  * 返回哈希表key中的所有域
  *
  * @param string key 哈希表key
@@ -64,6 +69,7 @@ $redis->hSet('member', 'mobile', '15920370150');
 var_dump($redis->hKeys('member'));  
  
 /*
+ * hVals
  * 返回哈希表key中的所有值
  *
  * @param string key 哈希表key
@@ -72,14 +78,16 @@ var_dump($redis->hKeys('member'));
 var_dump($redis->hVals('member'));
  
 /*
+ * hGetAll
  * 返回哈希表key中的所有域和值
  *
  * @param string key 哈希表key
  * @return 以列表形式返回哈希表的域和域的值。
  */
-var_dump($redis->hGetall('member'));  
+var_dump($redis->hGetAll('member'));  
  
 /*
+ * hExists
  * 查看哈希表key中，给定域field是否存在。 
  *
  * @param string key 哈希表key
@@ -91,6 +99,7 @@ var_dump($redis->hExists('member', 'name'));
 $redis->delete('member'); 
 
 /*
+ * hInrcBy
  * 为哈希表key中的域field的值加上增量increment。增量也可以为负数，相当于对给定域进行减法操作，如果key不存在，则或新创建一张哈希表，如果域存在并且值为数字则数字累加，如果为字符串则返回false，如果域不存在则添加该域。。
  *
  * @param string key 哈希表key
